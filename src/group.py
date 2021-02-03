bc007d87f9774b495afb8fe12b90dad9dacd1acb
Izaak van Dongen <izaak.van.dongen@gmail.com>
2021-02-03 01:21:17 +0000

    Make some drastic unsolicited changes
    Particularly, rewrite Perms to work with lists and tuples of a fixed
    length. This allows us to make hashing much much faster, which speeds up
    this program a lot.
    
    Also just faff around a bit with coding style and output format.

--- src/group.py
+++ src/group.py
@@ -7,114 +7,94 @@ Group class
 import math, itertools
 from perm import Perm
 
-def smallest_factor(n):
-    i = 2
-    while n % i != 0:
-        i += 1
-    return i
-
-
-def subsets(s):
-    """I don't care that this is bad"""
-    if len(s) == 0:
-        yield s
-        return
-    a = s.pop()
-    for t in subsets(s):
-        yield t
-        r = t.copy()
-        r.add(a)
-        yield r
-
-
-# Superceded by itertools.combinations
-def recursive_choose(s, n):
-    """I don't care that this is bad"""
-    if n < 0:
-        return
-    if len(s) == n:
-        yield s
-        return
-    a = s.pop()
-    yield from choose(s.copy(), n)
-    for t in choose(s.copy(), n-1):
-        t.add(a)
-        yield t
-
+# TODO: write dummy tests
 
 class Group:
     """
     An object representing a group, in the form of a set of permutations
     """
-    def __init__(self, perms=set()):
+    def __init__(self, perms={}):
         self.perms = perms
 
     def is_closed(self):
-        """Check that the group is closed under multiplication"""
-        return all(((perm1 * perm2 in self.perms) for perm1 in self.perms for
-                perm2 in self.perms))
+        """
+        Check that the group is closed under multiplication
+        """
+        return all(perm1 * perm2 in self.perms
+                    for perm1 in self.perms for perm2 in self.perms)
 
     def is_subgroup(self, other):
-        """Check if self is a subgroup of other (not if it's isomorphic to a
-        subgroup. If you know it's closed, just use perms.issubset"""
+        """
+        Check if self is a subgroup of other (not if it's isomorphic to a
+        subgroup. If you know it's closed, just use perms.issubset
+        """
         return other.perms.issubset(self.perms) and self.is_closed()
 
     @classmethod
     def cyclic(cls, n):
-        """Returns the cyclic group of order n"""
-        a = Perm.from_cycle((i for i in range(n)))
-        return cls({a**i for i in range(n)})
+        """
+        Returns the cyclic group of order n
+        """
+        return cls({tuple((j + i) % n for i in range(n)) for j in range(n)})
 
     @classmethod
     def dihedral(cls, n):
-        """Returns the dihedral group of order 2n"""
-        r = Perm.from_cycle((i for i in range(n)))
-        s = Perm({i:-i % n for i in range(n)})
-        return cls({a * (r ** i) for i in range(n) for a in {Perm(), s}})
+        """
+        Returns the dihedral group of order 2n
+        """
+        rots = cls.cyclic(n).perms
+        refls = {tuple((j - i) % n for i in range(n)) for j in range(n)}
+        return cls(rots | refls)
 
     @classmethod
     def symmetric(cls, n):
-        return cls.generate(Perm.from_cycle((1, 2)),
-                            Perm.from_cycle(tuple(range(1, n+1))))
+        return cls(set(map(Perm(n), itertools.permutations(range(n)))))
 
     def order_sequence(self):
-        """Return as a list the order sequence of the group"""
-        return sorted((a.order() for a in self.perms))
-
-    def on_ints(self, start=0):
-        """Convert all perms to be on the integers, starting at a given value
-
-        Returns a set of perms and the number of different integers used
         """
-        keys = {}
-        for perm in self.perms:
-            for key in perm.mapping.keys():
-                if key not in keys:
-                    keys[key] = start
-                    start += 1
+        Return list the order sequence of the group as a list
+        """
+        return sorted(a.order() for a in self.perms)
 
-        return {perm.apply_mapping(keys) for perm in self.perms}, start
+    # def on_ints(self, start=0):
+    #     """Convert all perms to be on the integers, starting at a given value
 
-    def direct_product(self, other):
-        """Returns the direct product of this group and another"""
-        elements1, start = self.on_ints()
-        elements2, _ = other.on_ints(start)
-        return type(self)({a * b for a in elements1 for b in elements2})
+    #     Returns a set of perms and the number of different integers used
+    #     """
+    #     keys = {}
+    #     for perm in self.perms:
+    #         for key in perm.mapping.keys():
+    #             if key not in keys:
+    #                 keys[key] = start
+    #                 start += 1
+    #     return {perm.apply_mapping(keys) for perm in self.perms}, start
+
+    # def direct_product(self, other):
+    #     """Returns the direct product of this group and another"""
+    #     elements1, start = self.on_ints()
+    #     elements2, _ = other.on_ints(start)
+    #     return type(self)({a * b for a in elements1 for b in elements2})
 
     def automorphism_group(self):
-        """Return the automorphism group of the group"""
+        """
+        Return the automorphism group of the group
+        """
         pass
 
     def is_isomorphic(self, other):
-        """Check if two groups are isomorphic, this is hard lol"""
+        """
+        Check if two groups are isomorphic, this is hard lol
+        """
         pass
 
     @classmethod
-    def generate(cls, *elements, limit=None):
-        """Generate more elements from some generating elements"""
+    def generate(cls, n, elements, limit=math.inf):
+        """
+        Generate more elements from some generating elements
+        """
         i = 1
-        Id = Perm()
-        G = cls(set([Id]))
+        Id = Perm(n)()
+        G = cls({Id})
         to_multiply = [Id]
         new = []
         while len(to_multiply) != 0:
@@ -123,7 +103,7 @@ class Group:
                     c = a * b
                     if c not in G.perms:
                         i += 1
-                        if limit is not None and i > limit:
+                        if i > limit:
                             return None
                         G.perms.add(c)
                         new.append(c)
@@ -141,21 +121,3 @@ class Group:
         #     if not self.generate_from(gh, *elements, limit=limit):
         #         return False
         # return True
-
-    @classmethod
-    def of_order(cls, n):
-        k = int(n/smallest_factor(n))
-        N = math.comb(math.factorial(n), k)
-        i = 0
-        for perms in itertools.combinations(cls.symmetric(n).perms, k):
-            i += 1
-            G = cls.generate(*perms, limit=n)
-            if G is not None and len(G.perms) == n:
-                yield G
-            print(i, "subsets checked out of", N)
-
-    @classmethod
-    def generated_by(cls, n):
-        """Subgroups of S_n generated by m elements"""
-        pass
-
--- src/main.py
+++ src/main.py
@@ -1,21 +1,50 @@
-from group import Group, subsets
-from perm import Perm
+from itertools import combinations
+from functools import reduce
+from operator import mul
+from math import factorial
+from time import time
+
+from group import Group
+
+n = 6
+UPDATE_INTERVAL = 10 ** 5
+
+def smallest_factor(n):
+    i = 2
+    while n % i != 0:
+        i += 1
+    return i
+
+# my version of PyPy is too old for math.comb
+def comb(n, k):
+    k = max(k, n - k)
+    return reduce(mul, range(k + 1, n + 1)) // factorial(n - k)
+
+def groups_of_order(n):
+    start_time = time()
+    k = int(n/smallest_factor(n))
+    N = comb(factorial(n), k)
+    total_groups = 0
+    for i, perms in enumerate(combinations(Group.symmetric(n).perms, k), 1):
+        G = Group.generate(n, perms, limit=n)
+        if G is not None and len(G.perms) == n:
+            yield G
+            total_groups += 1
+        if i % UPDATE_INTERVAL == 0 or i == N or i == 1:
+            elapsed = time() - start_time
+            print(f"{i:{len(str(N))}}/{N} ({i / N:7.2%}) {i / elapsed:.0f}/s"
+                  f" ETA{(N - i) * elapsed / i / 60:.0f}m"
+                  f" ({(N - i) * elapsed / i / 60 ** 2:.1f}h)"
+                  f" {total_groups}G")
 
 if __name__ == "__main__":
-    # H = Group.cyclic(3)
-    # K = G.direct_product(H)
-    # for perm in K.perms:
-    #     print(perm)
-    # H = Group.symmetric(6)
-    # for perm in H.perms:
-    #     print(perm)
+    H = Group.symmetric(6)
     # print(H.is_closed())
-    # print(len(H.perms))
-    i = 0
-    for G in Group.of_order(5):
-        i += 1
-        print("Group", i)
-        for perm in G.perms:
-            print(perm)
-        print()
+    print(len(H.perms))
+    for i, G in enumerate(groups_of_order(n)):
+        pass
+        # print("Group", i)
+        # for perm in G.perms:
+        #     print(perm)
+        # print()
 
